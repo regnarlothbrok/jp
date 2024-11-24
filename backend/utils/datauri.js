f2a2fd6272788116bbb7becc3799e5236eb71eac
Pratham Malviya <rjmalviya03@gmail.com>
2024-11-25 02:43:59 +0530

    latest commit 25 nov

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -3,6 +3,7 @@ import DataUriParser from "datauri/parser.js"
 import path from "path";
 
 const getDataUri = (file) => {
+    console.log("file:",file)
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer);
